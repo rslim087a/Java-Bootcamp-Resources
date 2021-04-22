0c3483404409183dbb9cf974dcb4823273d1836f
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-04-22 04:38:30 -0400

    Update Main.java

--- Module 2 - Object Oriented Programming/7. The World of Objects/dealership-full-code/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/dealership-full-code/Main.java
@@ -16,29 +16,29 @@ public class Main {
         Dealership dealership = new Dealership(cars);
         
 
-            System.out.println("\n ****** JAVA DEALERSHIP! ****** \n");
-            System.out.print("Welcome! Enter the type of car you're looking for: ");
-            String make = scan.nextLine();
-            System.out.print("Enter your budget: ");
-            int budget = scan.nextInt();          
+        System.out.println("\n ****** JAVA DEALERSHIP! ****** \n");
+        System.out.print("Welcome! Enter the type of car you're looking for: ");
+        String make = scan.nextLine();
+        System.out.print("Enter your budget: ");
+        int budget = scan.nextInt();          
 
-            int index = dealership.search(make, budget);
+        int index = dealership.search(make, budget);
 
-            switch (index) {
-                
-                case 404: System.out.println("Feel free to browse through our collection of cars.\n");
-                System.out.println(dealership);
-                break;
+        switch (index) {
 
-                default: scan.nextLine();
-                String decision = scan.nextLine();
-                if (decision.equalsIgnoreCase("yes")) {
-                    dealership.sell(index);
-                }
+            case 404: System.out.println("Feel free to browse through our collection of cars.\n");
+            System.out.println(dealership);
+            break;
 
+            default: scan.nextLine();
+            String decision = scan.nextLine();
+            if (decision.equalsIgnoreCase("yes")) {
+                dealership.sell(index);
             }
 
-        scan.close();
+        }
+
+    scan.close();
 
     }
 }
