a965c51ff8f6b37a2872e149f265efe8f1333aa3
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-04-23 02:57:33 -0400

    Update Machine.java

--- Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
@@ -1,3 +1,16 @@
 public class Machine {
     
+    
+    
+    /**
+     * Function name – dispense
+     * @param row (int)
+     * @param spot (int)
+     * @return (boolean)
+     * 
+     * Inside the function:
+     *  1. Checks if the requested item has a quantity bigger than 0.
+     *      • if so: decreases its quantity by one and returns true.
+     *      • otherwise: returns false.
+     */
 }
