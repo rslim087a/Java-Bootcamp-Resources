94e119b30f0d30b5b05c3d04e4d693552f6d679b
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-04-22 22:34:25 -0400

    Create Item.java

---
+++ Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine-solution/Item.java
@@ -0,0 +1,39 @@
+public class Item {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Item(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;   
+    }
+    public Item(Item source) {
+        this.name = source.name;
+        this.price = source.price;
+        this.quantity = source.quantity;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String toString() {
+        return this.name + ": " + this.price + " ("+this.quantity+")";
+    }
+
+}
