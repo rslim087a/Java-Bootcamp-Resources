f7b7404f7db96d53e2c20de7e080344f4d9272d4
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-05-29 05:05:55 -0400

    Create Functions.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-three (solution)/Functions.java
@@ -0,0 +1,22 @@
+public class Functions {
+    public static void main(String[] args) {
+        int random = random(15);    
+        System.out.println("Number: " + random);
+        System.out.println("Prime or Composite: " + (random == 1 ? "COMPOSITE" : isPrime(random)));
+    }
+
+    public static int random(int range) {
+        double random = Math.random() * range + 1;
+        return (int) random;
+    }
+    public static String isPrime(int random) {
+        String isPrime = "PRIME";
+        for (int i = 2; i < random; i++) {
+            if (random % i == 0) {
+                isPrime = "COMPOSITE";
+                break;
+            }
+        }
+        return isPrime;
+    }
+}
