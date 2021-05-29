fbdb8a2cbfe512bb07a7995b0be337e6ddba75fe
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-05-28 21:46:48 -0400

    Create Breakpoints.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Lessons/breakpoints/Breakpoints.java
@@ -0,0 +1,10 @@
+public class Breakpoints {
+    public static void main(String[] args) {
+        int number = 5;
+        long bigNumber = 50000000000L;
+        double decimal = 5.5;
+        boolean flag = true;
+        char letter = 'f';
+        String word = "hello";
+    }
+}
