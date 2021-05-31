ae413d88e931afd9bf4f8265bd029667a86782d3
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-05-31 05:10:57 -0400

    Create Pyramid.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-six (solution)/Pyramid.java
@@ -0,0 +1,16 @@
+public class Pyramid {
+
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (j < i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
