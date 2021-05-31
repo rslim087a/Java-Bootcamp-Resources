85989bb83536b79df9188903537491ddb411dac6
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-05-31 05:13:29 -0400

    Update Matrix.java

--- Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-seven (solution)/Matrix.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-seven (solution)/Matrix.java
@@ -9,7 +9,7 @@ public class Matrix {
         };
 
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 matrix[j][i] = i;
             }
         }
