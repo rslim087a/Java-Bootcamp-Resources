418d980a77151d69f26d8df2ecf0ed012625ff1c
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-06-13 16:23:40 -0400

    Create Machine.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/models/Machine.java
@@ -0,0 +1,39 @@
+package models;
+public class Machine {
+    private Item[][] items;
+
+    public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+    }
+    
+    public Item getItem(int row, int spot) {
+        return new Item(this.items[row][spot]);
+    }
+    
+    public void setItem(Item item, int row, int spot) {
+        this.items[row][spot] = new Item(item);
+    }
+
+    public void dispense(int row, int spot) {
+        items[row][spot].setQuantity(items[row][spot].getQuantity() - 1);
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < items.length; i++) {
+            temp += "\t";
+            for (int j = 0; j < items[i].length; j++) {
+                temp += items[i][j].toString() + " ";
+            }
+            temp += "\n\n";
+        }
+        temp += "\t************************************************";
+        return temp;
+    }
+
+}
