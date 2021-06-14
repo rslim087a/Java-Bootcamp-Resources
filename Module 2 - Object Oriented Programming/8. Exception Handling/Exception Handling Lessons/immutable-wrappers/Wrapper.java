2b0d355bfccb72cb410d1641dc422808f42a1b35
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-06-13 23:09:49 -0400

    Create Wrapper.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/immutable-wrappers/Wrapper.java
@@ -0,0 +1,8 @@
+public class Wrapper {
+    public static void main(String[] args) {
+        int wholeNumber = 5;
+        long largeWholeNumber = 5000000000L;
+        double decimal = 5.493;
+        char letter = 'b';
+    }
+}
