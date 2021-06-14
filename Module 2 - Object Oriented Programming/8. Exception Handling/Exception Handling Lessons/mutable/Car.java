dc9ba12d5a144980d0a7296eabb69e8e52fc215e
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-06-13 21:04:06 -0400

    Create Car.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/mutable/Car.java
@@ -0,0 +1,52 @@
+public class Car {
+    private String make;
+    private double price;
+
+    public Car(String make, double price) {
+        if (make == null || make.isBlank()) {
+            throw new IllegalArgumentException("make cannot be null/blank");
+        }
+        if (price < 0) {
+            throw new IllegalArgumentException("price cannot be less than zero");
+        }
+        this.make = make;
+        this.price = price;
+    }
+
+    public Car(Car source) { 
+            this.make = source.make;
+            this.price = source.price;    
+    }
+    public String getMake() {
+        return this.make;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setMake(String make) {
+        if (make == null || make.isBlank()) {
+            throw new IllegalArgumentException("make cannot be null/blank");
+        }
+        this.make = make;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("price cannot be less than zero");
+        }
+        this.price = price;
+    }
+
+    public void drive() {
+        System.out.println("\nYou bought the beautiful " + this.make + " for " + this.price + ".");
+        System.out.println("Please drive your car to the nearest exit.\n");
+    }
+
+    public String toString() {
+        return "\tMake: " + this.make + ".\n" 
+            +  "\tPrice: " + this.price + ".\n";
+    }
+
+}
