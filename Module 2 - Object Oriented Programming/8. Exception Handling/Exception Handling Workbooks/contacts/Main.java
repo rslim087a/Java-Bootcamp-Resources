62b3380f0a81dfb6ec133fa29c249e6f0b44cd4e
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-06-17 21:16:34 -0400

    Create Main.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        
+      
+        
+    }
+
+    /**
+     * Name: manageContacts
+     *
+     * Inside the function:
+     *   • 1. Starts a new instance of Scanner;
+     *   • 2. In an infinite loop, the user can choose to a) add b) remove a contact c) exit.
+     *   •        case a: ask for the name, phone number and birthDate.
+     *   •        case b: ask who they'd like to remove.
+     *   •        case c: break the loop.
+     *   • 3. close Scanner.
+     */
+
+
+
+    /**
+     * Name: loadContacts
+     * @param fileName (String)
+     * @throws FileNotFoundException
+     *
+     * Inside the function:
+     *   • 1. loads contacts from <fileName>;
+     *   • 2. From the manager object, it adds all contacts to the contacts list.
+     *        Hint: use scan.next to grab the next String separated by white space.
+     */
+
+}
