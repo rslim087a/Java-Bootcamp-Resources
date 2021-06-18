a9dc6fa46c84ae2f2b46bef4f1daa09622744afa
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-06-18 00:44:16 -0400

    Create Main.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/movie-store/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n********************JAVA VIDEO STORE********************\n");
+
+    }
+
+    /**
+     * Name: manageMovies
+     * Inside the function:
+     *   • 1. Starts a new instance of Scanner;
+     *   • 2. In an infinite loop, the user can choose to a) purchase b) rent c) return d) exit.
+     *   •        case a: ask for the name and sell.
+     *   •        case b: ask for the name and rent.
+     *   •        case c: ask for the name and return.
+     *   • 3. call close() from the Scanner object.
+     */
+
+
+    /**
+     * Name: loadMovies
+     * @param fileName (String)
+     * @throws FileNotFoundException
+     *
+     * Inside the function:
+     *   • 1. loads movies from <fileName>.txt.
+     *   • 2. adds all movies to the store object's movie field.
+     *        Hint: You will need to 'split' a String into three Strings.
+     */
+
+}
