5550e6b387727e91cb6c1dbe30a7bdcf5b488732
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-06-19 19:08:34 -0400

    Update Cart.java

--- Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Cart.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Cart.java
@@ -3,37 +3,38 @@ package models;
 public class Cart {
 
 
-    /**
-     * Name: add
-     * @param item
-     * 
-     * Inside the function:
-     *   1. Adds an item to the cart if it wasn't already added.
-     */
+   /**
+    * Name: add
+    * @param item
+    * @return boolean
+    *
+    * Inside the function:
+    *   1. Adds an item to the cart if it wasn't already added.
+    */
 
  
  
   
-    /**
-     * Name: remove
-     * @param name
-     * 
-     * Inside the function:
-     *   1. Removes the item that matches the name passed in.
-     */
+   /**
+    * Name: remove
+    * @param name
+    *
+    * Inside the function:
+    *   1. Removes the item that matches the name passed in.
+    */
 
  
  
   
-    /**
-     * Name: checkout
-     * @return (String)
-     * 
-     *  Inside the function:
-     *   1. Calculates the subtotal.
-     *   2. Calculates the tax (assume tax is 13%).
-     *   3. Calculates total: subtotal + tax 
-     *   4. Returns a String that resembles a receipt. See article for the String format.
-     */
+   /**
+    *  Name: checkout
+    *  @return (String)
+    *
+    *  Inside the function:
+    *   1. Calculates the subtotal (price before tax).
+    *   2. Calculates the tax (assume tax is 13%).
+    *   3. Calculates total: subtotal + tax
+    *   4. Returns a String that resembles a receipt. See below.
+    */
     
 }
