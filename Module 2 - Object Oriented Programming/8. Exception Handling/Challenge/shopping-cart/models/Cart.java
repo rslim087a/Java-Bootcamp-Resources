659a04c4daa484ca9d69ea06b9b48c6206b9e880
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-06-19 15:25:44 -0400

    Create Cart.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/models/Cart.java
@@ -0,0 +1,37 @@
+public class Cart {
+
+
+    /**
+     * Name: add
+     * @param item
+     * 
+     * Inside the function:
+     *   1. Adds an item to the cart if it wasn't already added.
+     */
+
+ 
+ 
+  
+    /**
+     * Name: remove
+     * @param name
+     * 
+     * Inside the function:
+     *   1. Removes the item that matches the name passed in.
+     */
+
+ 
+ 
+  
+    /**
+     * Name: checkout
+     * @return (String)
+     * 
+     *  Inside the function:
+     *   1. Calculates the subtotal.
+     *   2. Calculates the tax (assume tax is 13%).
+     *   3. Calculates total: subtotal + tax 
+     *   4. Returns a String that resembles a receipt. See article for the String format.
+     */
+    
+}
