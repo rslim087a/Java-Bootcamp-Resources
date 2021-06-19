457c5b84a1b95255ad7dff5955129e35e6f47b9e
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-06-19 15:23:36 -0400

    Create Main.java

---
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Challenge/shopping-cart/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    
+
+    public static void main(String[] args) {
+
+    
+    }
+
+    /**
+     * Name: manageItems
+     * Inside the function:
+     *   • 1. Starts a new instance of Scanner;
+     *   • 2. Creates an infinite loop:     
+     *   •        The user can choose to a) add or b) remove c) checkout.
+     *   •          case a: asks for the aisle and item number. Then, adds item to cart.
+     *   •          case b: asks for the name. Then, removes item from cart.
+     *   •          case c: prints the receipt and closes Scanner.
+     *   •        Prints the updated shopping cart.
+     */
+ 
+
+    /**
+     * Name: loadItems
+     * @param fileName (String)
+     * @throws FileNotFoundException
+     *
+     * Inside the function:
+     *   1. loads items from <fileName>.txt.
+     *      • while loop runs through every line in <fileName>
+     *      • scan.nextLine() picks up the entire line.
+     *      • splits each String using the ";" separator.
+     *      • splits both fields in each String using the "=" separator.
+     *      • Parse each price into a Double.
+     *   2. adds all items to the store object's items field.
+     */
+
+}
