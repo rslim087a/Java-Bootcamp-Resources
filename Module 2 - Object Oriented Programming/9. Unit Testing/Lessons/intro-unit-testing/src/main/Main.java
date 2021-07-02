4e88d0bdbef4bb0c8cc38eb63858733fef8cf889
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-07-01 23:26:17 -0400

    Create Main.java

---
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Lessons/intro-unit-testing/src/main/Main.java
@@ -0,0 +1,12 @@
+package src.main;
+
+public class Main {
+    
+    static double[] prices = new double[] {2.23, 1.32, 4.32, 11.33};
+
+    public static void main(String[] args) {   
+
+
+    }
+
+}
