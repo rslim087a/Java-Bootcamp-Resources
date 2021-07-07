359ecff56978bcad8f285544d604cb6516ffdef7
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-07-07 01:58:02 -0400

    Create Cart.java

---
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/shopping-cart/src/main/models/Cart.java
@@ -0,0 +1,33 @@
+package src.main.models;
+
+import java.util.ArrayList;
+
+public class Cart {
+    ArrayList<Item> items;
+
+    public Cart() {
+        this.items = new ArrayList<Item>();
+    }
+
+    public Item getItem(int index) {
+        return new Item(this.items.get(index));
+    }
+
+    public void setItem(int index, Item item) {
+        this.items.set(index, new Item(item));
+    }
+
+
+
+
+    
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < this.items.size(); i++) {
+            temp += this.items.get(i).toString();
+            temp += "\n";
+        }
+        return temp;
+    }
+
+}
