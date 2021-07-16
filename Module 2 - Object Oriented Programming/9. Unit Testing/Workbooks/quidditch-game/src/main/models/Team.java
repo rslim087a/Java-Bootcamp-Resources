b5467aae97ef78bd2165fff853d09f05c4d52e14
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-07-16 18:56:28 -0400

    Update Team.java

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
@@ -2,21 +2,21 @@ package src.main.models;
 
 public class Team {
 
-   private static final String POSITION_CHASER = "chaser";
-   private static final String POSITION_SEEKER = "seeker";
-   private static final String POSITION_KEEPER = "keeper";
+    private static final String POSITION_CHASER = "chaser";
+    private static final String POSITION_SEEKER = "seeker";
+    private static final String POSITION_KEEPER = "keeper";
 
 
-   public static String getPositionChaser() {
-       return POSITION_CHASER;
-   }
+    public static String getPositionChaser() {
+        return POSITION_CHASER;
+    }
 
-   public static String getPositionSeeker() {
-       return POSITION_SEEKER;
-   }
+    public static String getPositionSeeker() {
+        return POSITION_SEEKER;
+    }
 
-   public static String getPositionKeeper() {
-       return POSITION_KEEPER;
-   }
+    public static String getPositionKeeper() {
+        return POSITION_KEEPER;
+    }
 
 }
