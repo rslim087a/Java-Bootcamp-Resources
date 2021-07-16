93de6fd0f1ddee0f5b351bc732512d8a2a520788
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-07-16 14:00:36 -0400

    Create Employee.java

---
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Lessons/static/Employee.java
@@ -0,0 +1,34 @@
+public class Employee {
+    private String name;
+    private String position;
+
+    public Employee(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+  
+    public Employee(Employee source) {
+        this.name = source.name;
+        this.position = source.position;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getPosition() {
+        return position;
+    }
+  
+    public void greet(String company) {
+        if (company.equals("JAVASTARS")) {
+            System.out.println("Welcome!");
+        }
+    }
+
+
+    public String toString() {
+
+        return "\tName: " + name + "\tPosition: " + this.position + "\tCompany: " + COMPANY_NAME + "\n";
+    }
+}
