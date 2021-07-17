713d49c565c05fd51b3c74bd2448e8209f661802
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-07-16 21:22:06 -0400

    Create KeyValues.java

---
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Lessons/hashmap-starter/KeyValues.java
@@ -0,0 +1,8 @@
+public class KeyValues {
+  
+    public static void main(String[] args) {
+        String[] vegetables = new String[] {"Cauliflower", "Spaghetti Squash", "Parsley"};
+        double[] prices = new double[] {4.99, 1.99, 6.99};
+    }
+  
+}
