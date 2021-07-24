58939192613a20ed25295f95d5b37394b674ff9c
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-07-24 15:48:30 -0400

    Update Team.java

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
@@ -6,7 +6,7 @@ public class Team {
     private static final String POSITION_SEEKER = "seeker";
     private static final String POSITION_KEEPER = "keeper";
 
-//Question: the constants are final, so why can't we make them public? It's not possible for the caller to change it.
+//Question: the constants are final, so why can't we make them public? It's not possible for the caller to update them.
 //Answer: It's good practice to expose a method and not the variable, even if the constant is final. 
     
      public static String getPositionChaser() {
