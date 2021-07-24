36724c80e58e479ec96bb0b8d6c01abe9ec8f03d
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-07-24 15:44:29 -0400

    Update Team.java

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
@@ -2,24 +2,22 @@ package src.main.models;
 
 public class Team {
 
-    public static final String POSITION_CHASER = "chaser";
-    public static final String POSITION_SEEKER = "seeker";
-    public static final String POSITION_KEEPER = "keeper";
+    private static final String POSITION_CHASER = "chaser";
+    private static final String POSITION_SEEKER = "seeker";
+    private static final String POSITION_KEEPER = "keeper";
 
 
-/*If you decide to make the constants private, use the following getters.
-  Otherwise, you can remove all these comments. Both approaches are accepted.*/
     
-//     public static String getPositionChaser() {
-//         return POSITION_CHASER;
-//     }
+     public static String getPositionChaser() {
+         return POSITION_CHASER;
+     }
 
-//     public static String getPositionSeeker() {
-//         return POSITION_SEEKER;
-//     }
+     public static String getPositionSeeker() {
+         return POSITION_SEEKER;
+     }
 
-//     public static String getPositionKeeper() {
-//         return POSITION_KEEPER;
-//     }
+     public static String getPositionKeeper() {
+         return POSITION_KEEPER;
+     }
 
 }
