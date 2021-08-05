dd5d7f0e48dadca6a9bbf5aad3650899b356ce34
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-08-05 01:36:26 -0400

    Update Team.java

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
@@ -11,8 +11,7 @@ public class Team {
 Question: the constants are final, so why can't we make them public? It's not possible for the caller to update them.
   Answer: Even if the constant is final, I prefer to expose a method instead of the variable. But if you want to expose the variable, that's also correct.
 
-Question: isn't it better to use an enum instead of a list of constants?
-  Answer: Yes! But Enums haven't been covered yet. They are covered in the section: Inheritance and Polymorphism. */
+*/
 
     
      public static String getPositionChaser() {
