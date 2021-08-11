672447fdc221508872b47732fadabadba8639b85
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-08-11 19:13:12 -0400

    Update Person.java

--- Module 2 - Object Oriented Programming/10. Inheritance/intro-to-inheritance/Person.java
+++ Module 2 - Object Oriented Programming/10. Inheritance/intro-to-inheritance/Person.java
@@ -13,21 +13,4 @@ public class Person {
         this.age = source.age;
     }
 
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
 }
