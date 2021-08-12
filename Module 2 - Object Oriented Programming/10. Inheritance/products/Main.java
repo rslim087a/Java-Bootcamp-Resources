8cbe2db5710fb10b00d7b1075e819f35b452fd76
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-08-12 00:04:07 -0400

    Create Main.java

---
+++ Module 2 - Object Oriented Programming/10. Inheritance/products/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+  
+    static final String FILE_NAME = "products.txt";
+
+    public static void main(String[] args) {
+
+    }
+  
+    /**
+     * Function Name: getData
+     * @return Product[]
+     * @throws FileNotFoundException
+     * 
+     * Inside the function:
+     *   1. Loads the data from products.txt
+     */
+}
