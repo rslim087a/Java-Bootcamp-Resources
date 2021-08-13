b315f0abfdd570389ec670f6a1428551ea7ae971
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-08-13 00:18:09 -0400

    Create Main.java

---
+++ Module 2 - Object Oriented Programming/10. Inheritance/polymorphism-lists/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);        
+    }
+}
