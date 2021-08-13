0ccccc80ce1115578157b0c1dfba2afe9a33eaa2
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-08-12 23:52:12 -0400

    Create SortingStrings.java

---
+++ Module 2 - Object Oriented Programming/10. Inheritance/string-comparable/SortingStrings.java
@@ -0,0 +1,8 @@
+public class SortingStrings {
+    public static void main(String[] args) {
+        String john = "John";
+        String amy = "Amy";
+        String jane = "Jane";
+        
+    }
+}
