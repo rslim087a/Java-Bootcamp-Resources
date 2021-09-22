943dd1fb9ba4593ba70833b77355e10754b55e32
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-09-21 22:23:41 -0400

    Update Main.java

--- Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.6/Main.java
+++ Module 2 - Object Oriented Programming/11. Concurrency/Workbooks/Workbook 11.6/Main.java
@@ -3,7 +3,7 @@ public class Main {
     static String[] files = new String[] { "data/sales1.csv", "data/sales2.csv", "data/sales3.csv"};
    
     static int sampleSize = 0;
-    static double quantitySold = 0;
+    static int quantitySold = 0;
 
 
     public static void main(String[] args) throws Exception {
