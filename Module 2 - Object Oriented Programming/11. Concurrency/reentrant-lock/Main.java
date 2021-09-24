6ff9697018bd57939cfb5b3d6c5c116a035aca22
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-09-23 23:14:22 -0400

    Update Main.java

--- Module 2 - Object Oriented Programming/11. Concurrency/reentrant-lock/Main.java
+++ Module 2 - Object Oriented Programming/11. Concurrency/reentrant-lock/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     
+    static int counter = 0;
+    
     public static void main(String[] args) {
     
+    }
+    
+    public static void increment() {
+        counter++;
     }
   
 }
