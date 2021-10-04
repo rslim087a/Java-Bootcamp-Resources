2c1e85218c8952586f9d38c8cc9aad8814001d06
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-10-03 22:29:48 -0400

    Create Color.java

---
+++ Module 2 - Object Oriented Programming/Capstone/solution/stock-trader/src/main/utils/Color.java
@@ -0,0 +1,9 @@
+package src.main.utils;
+
+public class Color {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+}
