736c9da429dda9c3d0d22cf875408443ac4ea3f7
rslim087a <42172016+rslim087a@users.noreply.github.com>
2021-10-03 21:53:46 -0400

    Create Main.java

---
+++ Module 2 - Object Oriented Programming/Capstone/starter/stock-trader/src/main/Main.java
@@ -0,0 +1,14 @@
+package src.main;
+
+public class Main {
+
+    static Account account; 
+    static final double INITIAL_DEPOSIT = 4000;
+    static Scanner scanner = new Scanner(System.in);
+  
+    public static void main(String[] args) {    
+
+      
+    }
+
+}
