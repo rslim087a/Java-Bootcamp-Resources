46048a87f77142d1e2fb77c53e52a129581b1680
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-05-18 18:57:57 -0400

    Update Functions.java

--- Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-three (solution)/Functions.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-three (solution)/Functions.java
@@ -2,7 +2,7 @@ public class Functions {
     public static void main(String[] args) {
         int random = random(15);    
         System.out.println("Number: " + random);
-        System.out.println("Prime or Composite: " + (random == 1 ? "COMPOSITE" : isPrime(random)));
+        System.out.println("Prime or Composite: " + (random == 1 ? "NEITHER" : isPrime(random)));
     }
 
     public static int random(int range) {
