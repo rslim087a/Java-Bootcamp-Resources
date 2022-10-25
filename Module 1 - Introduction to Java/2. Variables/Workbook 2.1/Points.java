652d6569162c7959bf458ef899b7bf21ca0ad528
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-24 22:34:31 -0400

    Update Points.java

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.1/Points.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.1/Points.java
@@ -1,7 +1,7 @@
 public class Points {
     public static void main(String[] args) {
         
-        //See Learn the Part for the complete instructions. 
+         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
         int points = 0;
 
