f296ecd049c2abfe80693cbc786972f667c66827
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-24 22:35:28 -0400

    Update WorkSchedule.java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -1,5 +1,8 @@
 public class WorkSchedule {
     public static void main(String[] args) {
+        
+        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+
         int day = 3;   //3rd day of the week...
         boolean holiday = true;
          
