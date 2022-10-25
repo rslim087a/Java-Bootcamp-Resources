7cf85be29934dccc75d045be1104649fcffb5b87
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-24 22:36:07 -0400

    Update Championship.java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -1,6 +1,8 @@
 public class Championship {
     public static void main(String[] args) {
 
+        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+
         int gryffindor = 400;    //gryffindor points
         int ravenclaw = 200;    //ravenclaw points
 
