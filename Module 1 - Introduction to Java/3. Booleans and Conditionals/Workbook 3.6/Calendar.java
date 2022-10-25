438ba4d11b1c1f974a9200ba78ce9fa9263ea0f9
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-24 22:35:11 -0400

    Update Calendar.java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
@@ -1,5 +1,8 @@
 public class Calendar {
     public static void main(String[] args) {
+        
+        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+
         String day = "Friday"; //Can be any day.
         System.out.println("Hey, are you free on " + day + "? \n");
 
