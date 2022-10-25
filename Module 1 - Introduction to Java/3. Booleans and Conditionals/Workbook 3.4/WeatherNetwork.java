4c9169fbda7cd840883ba6d786a45173ebafb0eb
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-24 22:35:48 -0400

    Update WeatherNetwork.java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
@@ -1,6 +1,9 @@
 public class WeatherNetwork {
     public static void main(String[] args) {
 
+        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+
+        
         int temp = 25;  
 
         String forecast = "";
