7cc6dc9bd714520427f7c0bb0fc1345e1fc354da
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-28 13:59:02 -0400

    Update WorkSchedule.java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -6,9 +6,9 @@ public class WorkSchedule {
         int day = 3;   //3rd day of the week...
         boolean holiday = true;
          
-        // if it's a holiday, print: "woohoo, no work!");
-        // if it's the weekend, print: "it's the weekend, no work!"
-        // otherwise, print: "Wake up at 7:00 :( ";
+        // IF - ELSE IF - ELSE HERE!
+        
+        
         }
 
     }
