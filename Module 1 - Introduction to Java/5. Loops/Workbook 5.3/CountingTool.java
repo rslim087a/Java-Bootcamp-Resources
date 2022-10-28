7a552ef844271284f994aae1305744138efb0ef9
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-28 03:31:04 -0400

    Update CountingTool.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.3/CountingTool.java
@@ -4,19 +4,7 @@ public class CountingTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
-        /*Task 1 – Choose a number
-
-            Ask: Hi Timmy! Choose a number to count to: 
-
-            Make sure Timmy can enter the value BESIDE the question. 
-        */
-
-        /* Task 2 – Count from 0 to that number
-
-             Let's say Timmy entered 5, you would display: 0 1 2 3 4 5 
-
-        */
+        // See Learn the Part for the instructions.
 
         scan.close();
     }
