fba49c9516925c8f6648f5240a5eac808aeb1794
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-28 02:59:13 -0400

    Update EvenOrOdd.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.6/EvenOrOdd.java
@@ -1,13 +1,5 @@
 public class EvenOrOdd {
     public static void main(String[] args) {
-        // Task 1 – Make a for loop that counts from 0 to 19. 
-        
-        /* Task 2
-             
-            1. If the number is even, print ' – even' beside the number.
-            2. If the number is odd, print ' – odd' beside the number.
-
-         */
-
+        // See Learn the Part for detailed instructions.
     }
 }
