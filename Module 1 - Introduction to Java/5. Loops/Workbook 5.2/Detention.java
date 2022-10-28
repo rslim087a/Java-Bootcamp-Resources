d4ba21208cba4a5407a0b53bc083c2953bb9b5f8
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-28 01:31:11 -0400

    Update Detention.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.2/Detention.java
@@ -2,7 +2,7 @@ public class Detention {
     public static void main(String[] args) {
 
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
-        //Task 1 – Scan for the nextLine(), and print it 99 times. 
+        // See Learn the Part for the instructions.
         
     }
 }
