d9af37490336a93bf85023a38b7b6f9d123e1cda
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-28 04:22:05 -0400

    Update SignIn.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.9 Solution/SignIn.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.9 Solution/SignIn.java
@@ -7,16 +7,16 @@ public class SignIn {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Javagram! Sign in below\n");
-        System.out.print("• Username: ");
+        System.out.print("- Username: ");
         String usernameEntry = scan.nextLine();
-        System.out.print("• Password: ");
+        System.out.print("- Password: ");
         String passwordEntry = scan.nextLine();
 
         while (!usernameEntry.equals(username) || !passwordEntry.equals(password)) {
             System.out.println("\nIncorrect, please try again!\n");
-            System.out.print("• Username: ");
+            System.out.print("- Username: ");
             usernameEntry = scan.nextLine();
-            System.out.print("• Password: ");
+            System.out.print("- Password: ");
             passwordEntry = scan.nextLine();
         }
 
