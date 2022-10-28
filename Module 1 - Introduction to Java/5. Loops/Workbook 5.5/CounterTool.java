6d7d2a36fd4dcf685f5dbbe658df8540154cd0a5
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-28 02:44:20 -0400

    Update CounterTool.java

--- Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
+++ Module 1 - Introduction to Java/5. Loops/Workbook 5.5/CounterTool.java
@@ -7,26 +7,7 @@ public class CounterTool {
         System.out.println("Jimmy: It depends.");
         System.out.println("Oh, ok...");
 
-      /* Task 1 – Get integers from the user.
-
-
-          1.   print : Pick a number to count by:
-               pick up the user's answer
-        
-          2.   print : Pick a number to start counting from:
-               pick up the user's answer
-
-          3.    print : Pick a number to count to:
-                pick up the user's answer 
-                
-        */
-        
-
-      /* Task 2 – Create a for loop that:
-            1. starts from the second number.
-            2. stops at the third number. 
-            3. counts in steps of the first.
-      */
+       // See detailed instructions on Learn the Part.
 
         scan.close();
     }
