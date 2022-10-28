483a01c2365734a65b8809bfe17fb0da22aa04e0
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-28 13:59:41 -0400

    Update Calendar.java

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.6/Calendar.java
@@ -9,18 +9,7 @@ public class Calendar {
 
         System.out.println("Hmm, let me check my calendar.");
         
-        //Check calendar here:
-
-            //  "Monday": print ("Sorry, I have to stay at work late."); 
-            //  "Tuesday": print ("It looks like I have meetings all day."); 
-            //  "Wednesday": print ("I have a dentist appointment. Some other time!"); 
-            //  "Thursday": print ("Sorry, thursday is date night!"); 
-            //  "Friday": print ("I'm free!!"); 
-            //  "Saturday": print ("I'm free!!"); 
-            //  "Sunday": print ("I'm free!!"); 
-            //  "otherwise": print ("that's not a day."); 
-
-        
+        //Check 'calendar' here:        
         
 
     }
