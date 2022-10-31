8db694b67acc92eeb9ffa8fb7a70da3687166efa
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-30 21:09:42 -0400

    Update FirstProgram.java

--- Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
+++ Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -1,2 +1,2 @@
 // I'm a comment. I have no impact on your code.
-// Instructions for this workbook are on Learn the Part: (See the Udemy Video: Workbook 1.1 to access the link).
+// Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
