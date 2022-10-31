84cfc98b0f9c6df85d4d157f41a23719263f2a91
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-30 21:08:31 -0400

    Update YourInitials.java

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,5 @@
 public class YourInitials {
     public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part: (See the Udemy Video: Your Initials to access the link).
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
     }
 }
