654a62d10c51f95004607550d1e95ea36092c640
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-31 02:11:58 -0400

    Update SignUp.java

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.5/SignUp.java
@@ -3,42 +3,30 @@ import java.util.Scanner;
 public class SignUp {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);        
 
         System.out.println("Welcome to JavaGram! Let's sign you up.");
         
-         //See Learn the Part for the complete instructions.
+        System.out.println("What is your first name?");
+        
+        System.out.println("What is your last name?");
 
+        System.out.println("How old are you?");
 
-        //Task 1 - Using Scanner, ask the user questions
+        System.out.println("Make a username");  
 
-        //Ask for their first name.
-        //Ask for their last name.
-        //Ask: how old are you?
-        /* Ask them to make a username. SIDE NOTE:
-           – nextLine() gets "skipped" if you put it after nextInt() , nextDouble(), nextLong().
-           – You will understand why this happens when we cover Delimiters in the next section (Booleans and Conditionals).
-           – For now, just know that the solution is to add an extra nextLine().
-        */
-        //Ask what city they live in.
-        //Ask what country that's from.
+        System.out.println("What city do you live in?");
 
-
-        //Task 2 - Print their information. 
-
+        System.out.println("What country is that?");
+        
         System.out.println("Thank you for joining JavaGram!");
-
-        //Print their information like so:
-
-        // Your information:
-        //        First Name: Rayan
-        //        Last Name: Slim
-        //        Age: 27
-        //        Username: monotonic_relu
-        //        City: Ottawa
-        //        Country: Canada
-        //
-
+        System.out.println("\nHere is the information you entered:");
+        System.out.println("\tFirst Name: <firstName>");
+        System.out.println("\tLast Name: <lastName>");
+        System.out.println("\tAge: <age>");
+        System.out.println("\tUsername: <userName>");
+        System.out.println("\tCity: <city>");
+        System.out.println("\tCountry: <country>");
 
         //close scanner. It's good practice :D ! 
     }
