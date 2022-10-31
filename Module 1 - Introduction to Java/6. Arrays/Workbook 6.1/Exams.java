5778eb0d0749f6e74e8c42bc3aa900ca6bab616d
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-31 19:22:20 -0400

    Update Exams.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
@@ -1,11 +1,15 @@
 public class Exams {
     public static void main(String[] args) {
-        
-        System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
-        //Task 1: Create an array that stores 5 students: "Harry", "Neville", "Ron", "Hermione", "Seamus"
 
-        //Task 2: Assign each student a seat according to their index in the array.
-        //           print: " <student at index 0> ", you will take seat 0. 
+        // Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 6.1 to access the link).
+
+        System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
+        
+        System.out.println("<student at index 0>, you will take seat 0");
+        System.out.println("<student at index 1>, you will take seat 1");
+        System.out.println("<student at index 2>, you will take seat 2");
+        System.out.println("<student at index 3>, you will take seat 3");
+        System.out.println("<student at index 4>, you will take seat 4");
 
     }
 }
