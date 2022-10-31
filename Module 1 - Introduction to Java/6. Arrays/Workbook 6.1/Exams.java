e237c97ee057959f5e63ce7fe61c7e10f7a11ae1
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-31 19:59:37 -0400

    Update Exams.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.1/Exams.java
@@ -5,11 +5,11 @@ public class Exams {
 
         System.out.println("It's time to take your 5th year exams. Please, take your seats.\n");
         
-        System.out.println("<student at index 0>, you will take seat 0");
-        System.out.println("<student at index 1>, you will take seat 1");
-        System.out.println("<student at index 2>, you will take seat 2");
-        System.out.println("<student at index 3>, you will take seat 3");
-        System.out.println("<student at index 4>, you will take seat 4");
+        System.out.println("<student at index 0>, you will take seat " + 0);
+        System.out.println("<student at index 1>, you will take seat " + 1);
+        System.out.println("<student at index 2>, you will take seat " + 2);
+        System.out.println("<student at index 3>, you will take seat " + 3);
+        System.out.println("<student at index 4>, you will take seat " + 4);
 
     }
 }
