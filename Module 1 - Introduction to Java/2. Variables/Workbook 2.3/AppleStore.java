dfb155de3368da5ebd9edc7c3d03f1cc5ee88e86
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-31 01:01:30 -0400

    Update AppleStore.java

--- Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStore.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStore.java
@@ -4,7 +4,7 @@ public class AppleStore {
         int numOfCustomers = 0;
         double profit = 0;
 
-        // Instructions for this workbook are on Learn the Part: (See the Udemy Video: Workbook 2.3 to access the link).
+        // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.3 to access the link).
 
         System.out.println("You picked 500 apples from an apple orchard");
         
