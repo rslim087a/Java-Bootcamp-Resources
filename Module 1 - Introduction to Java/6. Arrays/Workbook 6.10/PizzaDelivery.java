6f1d0eb3b92ffd7a594dfe774046dbedcb65f8dc
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-11-01 16:54:44 -0400

    Update PizzaDelivery.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.10/PizzaDelivery.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.10/PizzaDelivery.java
@@ -2,14 +2,29 @@ import java.util.Scanner;
 
 public class PizzaDelivery {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         
         // See instructions on Learn the Part (Workbook 6.10)
+
+        Scanner scan = new Scanner(System.in);
+        
         System.out.println("How many pizza toppings do you want?");
+        int numToppings = scan.nextInt(); 
+        scan.nextLine(); // Throwaway nextLine() to avoid nextLine() trap. Since it comes after nextInt(), it will just get skipped anyway.
         
         System.out.println("Great, enter each toppping!");
-
+        
+        String[] toppings = new String[numToppings]; // Length of array equals what the user inputs.
+        
+        // Task 1 here
+        
         System.out.println("\nThank you! Here are the toppings your ordered");
+        
+        // Task 2 here
+        
+        System.out.println("\nPress anything to confirm your order.");
+        scan.nextLine();
+        System.out.println("Great, a driver is on the way!");
+
         scan.close();
 
 
