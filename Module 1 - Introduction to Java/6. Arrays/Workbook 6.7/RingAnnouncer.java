c6401f5993b30badd8be6a6f8b92d980448ba3e6
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-11-01 00:13:11 -0400

    Update RingAnnouncer.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.7/RingAnnouncer.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.7/RingAnnouncer.java
@@ -1,24 +1,12 @@
 public class RingAnnouncer {
     public static void main(String[] args) {
         String[] record = {"WIN", "WIN", "WIN", "LOSS", "WIN", "WIN", "LOSS"};
+        
+        // Instructions for this workbook are on Learn the Part (Workbook 6.7).
+        
+        System.out.println("\nWith a professional record of <wins> Wins and <losses> losses.");
+        System.out.println("He is the pride of oracle: Java Fury!");
 
-        /** Task 1 
-         * 
-         *   Using a for loop:
-         *       count Java's wins and store the result in a variable: wins. 
-         *       count Java's losses and store the result in a variable: losses.
-         *
-         * 
-         * 
-         */
-
-        /** Task 2
-         * Print:
-         *     • \nWith a professional record of <wins> Wins and <losses> losses. 
-         *     • He is the pride of oracle: Java Fury!
-         *
-         * 
-         */        
 
     }
 }
