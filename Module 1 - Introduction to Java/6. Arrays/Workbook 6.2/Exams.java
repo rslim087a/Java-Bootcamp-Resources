80ef5ad0c67d2305a452d3ff26db55d3425fb2ba
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-11-01 00:07:17 -0400

    Update Exams.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.2/Exams.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.2/Exams.java
@@ -1,7 +1,7 @@
 public class Exams {
     public static void main(String[] args) {
         
-        // Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 6.2 to access the link).
+        // Instructions for this workbook are on Learn the Part (Workbook 6.2).
         System.out.println("It's time to take your 5th years exams. Please, take your seats.\n");
         
         System.out.println("<student at index i>, you will take seat <i>");
