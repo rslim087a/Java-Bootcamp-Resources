6824c0b08426174fc9e5cdaa88c3f682a9ee2f7c
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-10-31 23:42:32 -0400

    Update HighScore.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.6/HighScore.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.6/HighScore.java
@@ -14,12 +14,12 @@ public class HighScore {
                 highScore = scores[i];
             }
         }
-        /** Task 1 – Find the person with the highest score
-         *  
-         *   • After obtaining the highest score, print: The highest score is:  <highScore>. Impressive!
-         *   • It's the gentleman in seat: <index>. Give that man a cookie!
-         *  
-         */
+        
+       
+        // TODO : See instructions on Learn the Part (Workbook 6.6)
+        System.out.println(" - After obtaining the highest score, print: The highest score is:  <highScore>. Impressive!");
+        System.out.println(" - It's the gentleman in seat: <index>. Give that man a cookie!");
+
     }
     
     public static int randomNumber() {
