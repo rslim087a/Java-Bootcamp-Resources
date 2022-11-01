2841331a4a2b9ba8e8b84de3354772dcacb71f38
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-11-01 00:39:31 -0400

    Update Tax.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.9/Tax.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.9/Tax.java
@@ -3,25 +3,10 @@ import java.util.Arrays;
 public class Tax {
     public static void main(String[] args) {
         double[] price = {1.99, 2.99, 3.99, 4.99};
-        /** Task 1: 
-         * 
-         *  Create a new array afterTax with the same length as price
-         * 
-         */
 
-         /** Task 2:
-          * 
-          *  Update each element in afterTax to be: price + 13% * price.
-          *
-          */
-
-         /** Task 3:
-          * 
-          * Print the following messages:
-          *     The original prices are: <original prices>
-          *     The prices after tax are: <after tax prices>
-          *
-          */
+        // See instructions on Learn the Part (Workbook 6.9)
+        System.out.println("The original prices are: <original prices>");
+        System.out.println("The prices after tax are: <after tax prices>");
 
     }
 }
