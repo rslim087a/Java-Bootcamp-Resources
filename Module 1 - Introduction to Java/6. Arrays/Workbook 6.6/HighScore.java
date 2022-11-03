cab68208c7c4ee9be50829866b8f9b5edc33a3fa
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-11-03 00:03:55 -0400

    Update HighScore.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.6/HighScore.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.6/HighScore.java
@@ -1,22 +1,18 @@
 public class HighScore {
     public static void main(String[] args) {
-        int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(),
-            randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
+        int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
         
-        System.out.print("Here are the scores: ");
+        int highScore = 0;
+        System.out.print("Here are the scores: ");    
         for (int i = 0; i < scores.length; i++) {
             System.out.print(scores[i] + " ");
-        }
-    
-        int highScore = 0;
-        for (int i = 0; i < scores.length; i++) {
             if (scores[i] > highScore) {
                 highScore = scores[i];
             }
         }
         
        
-        System.out.println(" - After obtaining the highest score, print: The highest score is:  " + highScore + ". Impressive!");
+        System.out.println(" - The highest score is: " + highScore + ". Impressive!");
         
         // TODO : See instructions on Learn the Part (Workbook 6.6)
         System.out.println(" - It's the gentleman in seat: <index>. Give that man a cookie!");
