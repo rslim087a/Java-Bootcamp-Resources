43ef0b11f940913094fe412760eb1eb7ab8499c6
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-11-02 21:26:51 -0400

    Update Prices.java

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.13/Prices.java
@@ -1,10 +1,5 @@
 public class Prices {
     public static void main(String[] args) {
-        //Task 1 - Create a 2D array that can store 3 rows and 5 columns of double values.
-
-        // Task 2 - Populate your 2D array with values from the table (see article)
-
-        // Task 3 - Print the prices for each department. See the article for the expected output.  
-
+        // The instructions for this workbook are on Learn the Part (workbook 6.13)
     }
 }
