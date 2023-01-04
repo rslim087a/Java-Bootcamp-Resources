cf93552186725f8854f30c523780bdd9e5d27c29
rslim087a <42172016+rslim087a@users.noreply.github.com>
2023-01-04 16:56:42 -0500

    Update Game.java

--- Module 2 - Object Oriented Programming/14. Enums/Workbook/starter/Game.java
+++ Module 2 - Object Oriented Programming/14. Enums/Workbook/starter/Game.java
@@ -1,5 +1,7 @@
 import java.time.LocalDate;
 import static constants.Regulation.*;
+import java.time.format.DateTimeFormatter;
+
 public class Game {
 
     private String arena;
@@ -11,7 +13,8 @@ public class Game {
     }
 
     public void begin(Team home, Team away) {
-        String formattedDate = this.date.getDayOfMonth() + "/" + this.date.getMonthValue() + "/" + this.date.getYear();
+//         String formattedDate = this.date.getDayOfMonth() + "/" + this.date.getMonthValue() + "/" + this.date.getYear();
+        String formattedDate = this.date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         System.out.println
         (
             "\n*****************************************************************************" +
